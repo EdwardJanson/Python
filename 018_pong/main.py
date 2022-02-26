5057f5ab8841edede4d61cb401e8b6afa64b0f5a
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-02-26 16:40:07 +0000

    Create main.py

---
+++ 018_pong/main.py
@@ -0,0 +1,34 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    score_keeping = ball.move(r_paddle, l_paddle)
+    if score_keeping == "r_loss":
+        scoreboard.l_point()
+    elif score_keeping == "l_loss":
+        scoreboard.r_point()
+
+screen.exitonclick()
