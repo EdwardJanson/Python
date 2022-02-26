4d5bb171c52175bd28f870c3af708461fa4d0022
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-02-26 07:05:09 +0000

    Update main.py

--- 017_snake_game/main.py
+++ 017_snake_game/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -17,3 +21,26 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 is_alive = True
+while is_alive:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.new_score()
+        snake.extend_snake()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_alive = False
+        scoreboard.game_over()
+
+    # Detect if head collides with parts in snake
+    for snake_part in snake.snake[1:]:
+        if snake.head.distance(snake_part) < 10:
+            is_alive = False
+            scoreboard.game_over()
+
+screen.exitonclick()
