a0b789f98b91849817b232669c483de4e9853d31
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-03-04 13:57:56 +0000

    Update main.py

--- 022_NATO_alphabet/main.py
+++ 022_NATO_alphabet/main.py
@@ -5,12 +5,6 @@ letter_dict = data.to_dict()
 letter_data_frame = pandas.DataFrame(letter_dict)
 ordered_dict = {row.letter: row.code for (index, row) in letter_data_frame.iterrows()}
 
-input_l = input("Type the word you would like to have in phonetic code. ")
-input_l_list = list(input_l)
-
-phonetic_code = []
-
-for l in input_l_list:
-    phonetic_code += [code for (letter, code) in ordered_dict.items() if letter.lower() == l]
-
+input_word = input("Type the word you would like to have in phonetic code. ").upper()
+phonetic_code = [ordered_dict[letter] for letter in input_word]
 print(phonetic_code)
