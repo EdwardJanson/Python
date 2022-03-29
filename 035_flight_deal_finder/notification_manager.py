8126583869096877f2b0468b36e771880db1208b
Edward Janson <99486978+edwardjanson@users.noreply.github.com>
2022-03-29 19:35:26 +0100

    Update notification_manager.py

--- 035_flight_deal_finder/notification_manager.py
+++ 035_flight_deal_finder/notification_manager.py
@@ -1,10 +1,14 @@
 from twilio.rest import Client
+import smtplib
 import os
 
 TWILIO_SID = os.environ.get("TWILIO_SID")
 TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
 TWILIO_VIRTUAL_NUMBER = os.environ.get("TWILIO_VIRTUAL_NUMBER")
 TWILIO_VERIFIED_NUMBER = os.environ.get("TWILIO_VERIFIED_NUMBER")
+MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
+EMAIL = os.environ.get("EMAIL_ADDRESS")
+PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 
 class NotificationManager:
@@ -18,5 +22,15 @@ class NotificationManager:
             from_=TWILIO_VIRTUAL_NUMBER,
             to=TWILIO_VERIFIED_NUMBER,
         )
-
         print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(EMAIL, PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
+                )
